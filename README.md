9bda5a76c481dc50002a8f0c2055272f54fffd92
Tom Ashworth <tashworth@twitter.com>
2016-08-02 17:37:12 +0100

    feat: initial commit of try-expression

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# if-expression
+# try-expression
 
-[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg?style=flat-square)](https://github.com/semantic-release/semantic-release) [![Build Status](https://travis-ci.org/phuu/if-expression.svg?branch=master)](https://travis-ci.org/phuu/if-expression)
+[![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg?style=flat-square)](https://github.com/semantic-release/semantic-release) [![Build Status](https://travis-ci.org/phuu/try-expression.svg?branch=master)](https://travis-ci.org/phuu/try-expression)
 
 JavaScript "if" as an expression.
 
 ## Install
 
 ```
-npm install --save if-expression
+npm install --save try-expression
 ```
 
 ## Use
@@ -15,9 +15,9 @@ npm install --save if-expression
 Run some code if the predicate is true:
 
 ```js
-import iff from "if-expression";
+import tryy from "try-expression";
 
-const message = iff(
+const message = tryy(
   true,
   () => "Cool!"
 ); // => Cool!
@@ -26,9 +26,9 @@ const message = iff(
 Run some other code if the predicate's false:
 
 ```js
-import iff from "if-expression";
+import tryy from "try-expression";
 
-const message = iff(
+const message = tryy(
   false,
   () => "Cool!",
   () => "Not so cool."
@@ -38,9 +38,9 @@ const message = iff(
 Check many things!
 
 ```js
-import iff from "if-expression";
+import tryy from "try-expression";
 
-const message = iff(
+const message = tryy(
   false,
   () => "Cool!",
   true,
@@ -52,9 +52,9 @@ const message = iff(
 Just return a value if everything is falsey:
 
 ```js
-import iff from "if-expression";
+import tryy from "try-expression";
 
-const message = iff(
+const message = tryy(
   false,
   () => "Cool!",
   false,
@@ -68,10 +68,10 @@ const message = iff(
 It can act like a statement too!
 
 ```js
-import iff from "if-expression";
+import tryy from "try-expression";
 
 let message;
-iff(true, () => {
+tryy(true, () => {
   message = "Cool!";
 }, () => {
   message = "Not so cool.";
--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-export default function iff(...args) {
-  // nada left to do
-  if (!args.length) {
-    return;
+export default function tryy(tryer = () => {}, catcher, finaliser) {
+  try {
+    return tryer();
+  } catch (err) {
+    return (
+      typeof catcher === 'function' ?
+        catcher(err) :
+        catcher
+    );
+  } finally {
+    if (typeof finaliser === 'function') {
+      finaliser();
+    }
   }
-  // else clause
-  if (args.length === 1 && typeof args[0] === 'function') {
-    return args[0]();
-  }
-  // default value
-  if (args.length === 1) {
-    return args[0];
-  }
-
-  // check the predicate
-  const [p, c, ...rest] = args;
-  return (p ? c() : iff(...rest));
 }
 
 if (process.env.NODE_ENV === 'test') {
@@ -24,45 +21,73 @@ if (process.env.NODE_ENV === 'test') {
     }
   };
 
+  const spy = ({
+    callCount: 0,
+    stub() {
+      spy.callCount += 1;
+    },
+    reset() {
+      spy.callCount = 0;
+    }
+  });
+
   assert(
-    iff(() => 1) === 1,
-    'else clause is always called if it\'s the only thing supplied'
+    tryy() === undefined,
+    'undefined is returned if nothing is supplied'
   );
 
   assert(
-    iff(
-      true,
-      () => 2,
-      () => 1
-    ) === 2,
-    'first clause is called if first predicate matches'
+    tryy(() => 1) === 1,
+    'tryer is called and returned if it does not throw'
   );
 
   assert(
-    iff(
-      false,
-      () => 3,
-      true,
-      () => 2,
-      () => 1
-    ) === 2,
-    'second clause is called if second predicate matches'
+    tryy(
+      () => {
+        throw new Error('Nope');
+      },
+      e => e.message
+    ) === 'Nope',
+    'catcher is called and returned if tryer throws'
   );
 
   assert(
-    iff(
-      false,
-      () => 1
-    ) === undefined,
-    'returns undefined if no clause matches'
+    tryy(
+      () => {
+        throw new Error('Nope');
+      },
+      'Meh'
+    ) === 'Meh',
+    'catcher value is returned if tryer throws'
   );
 
+  spy.reset();
   assert(
-    iff(
-      false,
+    tryy(
+      () => {
+        throw new Error('Nope');
+      },
+      () => 'Yep',
+      spy.stub
+    ) === 'Yep',
+    'catcher value is returned if finalizer is supplied and tryer throws'
+  );
+  assert(
+    spy.callCount === 1,
+    'finalizer was called if tryer throws'
+  );
+
+  spy.reset();
+  assert(
+    tryy(
       () => 1,
-      0
-    ) === 0,
-    'returns default value if no clause matches and value supplied'
+      e => e.message,
+      spy.stub
+    ) === 1,
+    'tryer value is returned if finalizer is supplied'
+  );
+  assert(
+    spy.callCount === 1,
+    'finalizer is called if supplied and tryer does not throw'
   );
 }
